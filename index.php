e9cd29ee4c244a1d2a296825af28c0f00336cf33
Hider <724598894@qq.com>
2017-11-20 21:33:28 +0800

    PHP后端

--- index.php
+++ index.php
@@ -5,4 +5,8 @@
  * Date: 2017/11/13
  * Time: 21:33
  */
+
+$fh = fopen('view/admin/msg.txt','r');
+
+
 require 'view/front/index.html';
--- view/admin/cron.php
+++ view/admin/cron.php
@@ -5,3 +5,7 @@
  * Date: 2017/11/19
  * Time: 10:57
  */
+
+$url='http://tool.mkblog.cn/movie/share.php';
+
+echo file_get_contents($url);
